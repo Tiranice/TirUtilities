aa58f3d7cb2364d68c21f858edf8069dd70a354c
Tiranice <35855236+Tiranice@users.noreply.github.com>
2024-07-15 12:48:15 -0500

    Adds new settings provider. Incomplete.

--- Assets/TirUtilities/Editor/Prefs/TirUtilitiesProjectSettings.cs
+++ Assets/TirUtilities/Editor/Prefs/TirUtilitiesProjectSettings.cs
@@ -6,14 +6,16 @@ namespace TirUtilities.Editor.Prefs
     using static ProjectEditorPaths;
     ///<!--
     /// TirUtilitiesProjectSettings.cs
-    /// 
+    ///
     /// Project:  TirUtilities
-    ///        
+    ///
     /// Author :  Devon Wilson
+    /// Compony:  Black Phoenix Creative
     /// Created:  Sep. 03, 2021
-    /// Updated:  Sep. 09, 2021
+    /// Updated:  May. 29, 2024
     /// -->
     /// <summary>
+    /// Controls the settings menu displayed in Unity's project settings window.
     /// </summary>
     /// <remarks>
     /// Based on the ProjectPreferences class in the source of RainbowFolders.
@@ -22,17 +24,25 @@ namespace TirUtilities.Editor.Prefs
     {
         #region String Constants
 
+        //==============================| Tooltips |==============================//
         private const string _HomeFolderTooltip = "Change this setting to the new location of the " +
                                                   "\"TirUtilities\" folder if you move it from its " +
                                                   "default location in the project.";
         private const string _AuthorNameTooltip = "This is the name that will be inserted into the" +
                                                   "placeholder in the script templates.";
+        private const string _AutoVersionNumberTooltip = "Toggles a build preprocessor that sets " +
+                                                         "the bundle version based on the git tags " +
+                                                         "and current branch.";
 
+        //================================| Keys |================================//
         private const string _HomeFolderPrefKey = "TirUtilities.HomeFolder.";
         private const string _AuthorNamePrefKey = "TirUtilities.AuthorName.";
+        private const string _AutoVersionNumberPrefKey = "TirUtilities.AutoVersionNumber.";
 
+        //==============================| Defaults |==============================//
         private const string _HomeFolderDefault = "Assets/TirUtilities";
         private const string _AuthorNameDefault = "AuthorName";
+        private const bool _AutoVersionNumberDefalut = false;
 
         #endregion
 
@@ -40,6 +50,7 @@ namespace TirUtilities.Editor.Prefs
 
         private static EditorPrefsString HomeFolderPref { get; }
         private static EditorPrefsString AuthorNamePref { get; }
+        private static EditorPrefsBool AutoVersionNumberPref { get; }
 
         #endregion
 
@@ -47,6 +58,7 @@ namespace TirUtilities.Editor.Prefs
 
         public static string HomeFolder { get; private set; }
         public static string AuthorName { get; private set; }
+        public static bool DoAutoVersionNumber { get; private set; }
 
         #endregion
 
@@ -54,17 +66,21 @@ namespace TirUtilities.Editor.Prefs
 
         static TirUtilitiesProjectSettings()
         {
-            var homeLabel = new GUIContent("Folder Location", _HomeFolderTooltip);
-            var authorLabel = new GUIContent("Author Name", _AuthorNameTooltip);
             HomeFolderPref = new EditorPrefsString($"{_HomeFolderPrefKey}{ProjectName}",
-                                                   homeLabel,
+                                                   new GUIContent("Folder Location", _HomeFolderTooltip),
                                                    _HomeFolderDefault);
 
             AuthorNamePref = new EditorPrefsString($"{_AuthorNamePrefKey}{ProjectName}",
-                                                   authorLabel,
+                                                   new GUIContent("Author Name", _AuthorNameTooltip),
                                                    _AuthorNameDefault);
+
+            AutoVersionNumberPref = new EditorPrefsBool($"{_AutoVersionNumberPrefKey}{ProjectName}",
+                                                        new GUIContent("Enable Auto Version Number", _AutoVersionNumberTooltip),
+                                                        _AutoVersionNumberDefalut);
+
             HomeFolder = HomeFolderPref;
             AuthorName = AuthorNamePref;
+            DoAutoVersionNumber = AutoVersionNumberPref;
         }
 
         #endregion
@@ -79,16 +95,15 @@ namespace TirUtilities.Editor.Prefs
                 guiHandler = searchContect =>
                 {
                     EditorGUILayout.LabelField("General", EditorStyles.boldLabel);
-                    EditorGUILayout.Separator();
-
                     HomeFolderPref.Draw();
 
                     EditorGUILayout.Separator();
-
                     EditorGUILayout.LabelField("Script Template Placeholders", EditorStyles.boldLabel);
-                    EditorGUILayout.Separator();
-                    
                     AuthorNamePref.Draw();
+
+                    EditorGUILayout.Separator();
+                    EditorGUILayout.LabelField("Build Pipeline", EditorStyles.boldLabel);
+                    AutoVersionNumberPref.Draw(200.0f);
                 }
             };
 
