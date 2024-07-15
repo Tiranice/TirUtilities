6c41fc1a24c411bb4d8e628dd83bebda89dea016
Tiranice <35855236+Tiranice@users.noreply.github.com>
2024-07-15 12:26:30 -0500

    Adds log methods

--- Assets/TirUtilities/Extensions/ObjectExtensions.cs
+++ Assets/TirUtilities/Extensions/ObjectExtensions.cs
@@ -4,12 +4,13 @@ namespace TirUtilities.Extensions
 {
     ///<!--
     /// ObjectExtensions.cs
-    /// 
+    ///
     /// Project:  TirUtilities
-    ///        
+    ///
     /// Author :  Devon Wilson
+    /// Company:  Black Phoenix Creative
     /// Created:  May 01, 2021
-    /// Updated:  May 28, 2021
+    /// Updated:  Jul 15, 2024
     /// -->
     /// <summary>
     /// A set of extensions to UnityEngine.Object.
@@ -18,5 +19,27 @@ namespace TirUtilities.Extensions
     {
         public static bool IsNull(this Object self) => self == null;
         public static bool NotNull(this Object self) => self != null;
+
+        public static bool IsNull(this Object self, Object debugContext)
+        {
+            if (self == null) UnityEngine.Debug.Log($"{self.name} was null in {debugContext}", debugContext);
+            return self == null;
+        }
+        public static bool IsNull(this Object self, Object debugContext, string remarks)
+        {
+            if (self == null) UnityEngine.Debug.Log($"{self.name} was null in {debugContext}:  {remarks}", debugContext);
+            return self == null;
+        }
+
+        public static bool NotNull(this Object self, Object debugContext)
+        {
+            if (self != null) UnityEngine.Debug.Log($"{self.name} not null in {debugContext}", debugContext);
+            return self != null;
+        }
+        public static bool NotNull(this Object self, Object debugContext, string remarks)
+        {
+            if (self != null) UnityEngine.Debug.Log($"{self.name} not null in {debugContext}:  {remarks}", debugContext);
+            return self != null;
+        }
     }
 }
