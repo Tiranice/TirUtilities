15f392dc1892d1948a9379d1108d95907f502145
Devon Wilson <devon.wilson@vanderbilt.edu>
2025-03-26 18:22:57 -0500

    Removes Unity 2020 compatibility

--- Assets/TirUtilities/Editor/Prefs/ScriptTemplateIntercept.cs
+++ Assets/TirUtilities/Editor/Prefs/ScriptTemplateIntercept.cs
@@ -45,12 +45,7 @@ namespace TirUtilities.Editor.Prefs.Experimental
             if (!File.Exists(Path.GetFullPath(path))) return;
 
             string fileContent = File.ReadAllText(path);
-#if !UNITY_2020_2_OR_NEWER
-            var rootNamespace = CompilationPipeline.GetAssemblyNameFromScriptPath(path);
-            if (rootNamespace == null || rootNamespace.Contains(".dll")) rootNamespace = EditorSettings.projectGenerationRootNamespace;
-            if (string.IsNullOrEmpty(rootNamespace)) rootNamespace = "RootNamespace";
-            fileContent = RemoveOrInsertNamespace(fileContent, rootNamespace);
-#endif
+
             fileContent = fileContent.Replace(ScriptTemplateKeys.DateToday, FormatedDate);
             fileContent = fileContent.Replace(ScriptTemplateKeys.ProjectName, PlayerSettings.productName);
             fileContent = fileContent.Replace(ScriptTemplateKeys.CompanyName, PlayerSettings.companyName);
@@ -62,69 +57,12 @@ namespace TirUtilities.Editor.Prefs.Experimental
         }
 
         private static string FormatedDate => $"{System.DateTime.Now:MMM dd, yyyy}";
-#if !UNITY_2020_2_OR_NEWER
-        private static string RemoveOrInsertNamespace(string content, string rootNamespace)
-        {
-            var rootNamespaceBeginTag = "#ROOTNAMESPACEBEGIN#";
-            var rootNamespaceEndTag = "#ROOTNAMESPACEEND#";
-
-            if (!content.Contains(rootNamespaceBeginTag) || !content.Contains(rootNamespaceEndTag))
-                return content;
-
-            if (string.IsNullOrEmpty(rootNamespace))
-            {
-                content = Regex.Replace(content, $"((\\r\\n)|\\n)[ \\t]*{rootNamespaceBeginTag}[ \\t]*", string.Empty);
-                content = Regex.Replace(content, $"((\\r\\n)|\\n)[ \\t]*{rootNamespaceEndTag}[ \\t]*", string.Empty);
-
-                return content;
-            }
-
-            // Use first found newline character as newline for entire file after replace.
-            var newline = content.Contains("\r\n") ? "\r\n" : "\n";
-            var contentLines = new List<string>(content.Split(new[] { "\r\n", "\r", "\n" }, System.StringSplitOptions.None));
-
-            int i = 0;
-
-            for (; i < contentLines.Count; ++i)
-            {
-                if (contentLines[i].Contains(rootNamespaceBeginTag))
-                    break;
-            }
-
-            var beginTagLine = contentLines[i];
-
-            // Use the whitespace between beginning of line and #ROOTNAMESPACEBEGIN# as identation.
-            var indentationString = beginTagLine.Substring(0, beginTagLine.IndexOf("#"));
-
-            contentLines[i] = $"namespace {rootNamespace}";
-            contentLines.Insert(i + 1, "{");
-
-            i += 2;
-
-            for (; i < contentLines.Count; ++i)
-            {
-                var line = contentLines[i];
-
-                if (System.String.IsNullOrEmpty(line) || line.Trim().Length == 0)
-                    continue;
-
-                if (line.Contains(rootNamespaceEndTag))
-                {
-                    contentLines[i] = "}";
-                    break;
-                }
-
-                contentLines[i] = $"{indentationString}{line}";
-            }
-
-            return string.Join(newline, contentLines.ToArray());
-        }
-#endif
     }
-
 
     public readonly ref struct ScriptTemplateKeys
     {
+        //TODO:  Add license field
+
         public static string ProjectName => "#PROJECT#";
         public static string AuthorName => "#AUTHOR#";
         public static string CompanyName => "#COMPANY#";
